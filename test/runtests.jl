a69039f6b8a8f328b79961a20308abd9809a8330
Sebastian Stock <42280794+sostock@users.noreply.github.com>
2020-11-05 14:17:34 +0100

    testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,14 @@ using Unitful
 using UnitfulEquivalences
 using Test
 
-@testset "Equivalences" begin
-    # MassEnergy
+@testset "MassEnergy" begin
     @test uconvert(u"keV", 1u"me", MassEnergy()) ≈ 510.999u"keV" (atol = 0.001u"keV")
     @test uconvert(u"kg", 1000u"keV", MassEnergy()) ≈ 1.957u"me" (atol = 0001u"me")
 
-    # PhotonEnergy
+    @test_throws ArgumentError uconvert(u"ms^-1", 1u"m", MassEnergy())
+end
+
+@testset "PhotonEnergy" begin
     @test uconvert(u"eV", 589u"nm", PhotonEnergy()) === 2.104994880020378u"eV"
     @test uconvert(u"μm", 1u"eV", PhotonEnergy()) ≈ 1.239u"μm" (atol = 0.001u"μm")
 
@@ -17,7 +19,5 @@ using Test
     @test uconvert(u"eV", 1u"fs^-1", PhotonEnergy()) === 4.135667696923859u"eV"
     @test uconvert(u"ns^-1", 1u"eV", PhotonEnergy()) === 241798.9242084918u"ns^-1"
 
-    # Throw errors
     @test_throws ArgumentError uconvert(u"kg", 1u"eV", PhotonEnergy())
-    @test_throws ArgumentError uconvert(u"ms^-1", 1u"m", MassEnergy())
 end
